11715f94f7d904210f2b9ec9fd867a7929aa577c
Carlos Del Cura Pascual <delcuracarlos@gmail.com>
2020-01-28 13:49:55 +0100

    Stiky menu and responsive header and nav

--- JS/script.js
+++ JS/script.js
@@ -3,20 +3,38 @@ $(window).load(function() {
     $(".cargador").fadeOut("slow");
 });
 
- let textoClaseUl = document.querySelectorAll("aside > ul:first-child > li > a");
- let vacio = "";
+//Sscroll menú
+window.onscroll = () => {menu()};
+window.addEventListener('load', () =>{
+    $(this).scrollTop(0);
+});
+
+let textoClaseUl = document.querySelectorAll("aside > ul:first-child > li > a");
+let vacio = "";
 //  console.log(textoClaseUl);
 for (let index = 0; index < textoClaseUl.length; index++) {
     textoClaseUl[index].firstChild.textContent = "";
 }
 // console.log(textoClaseUl);
 
-let textoH1 = document.querySelectorAll("header > nav > h1 > a")[0];
+let textoH1 = document.querySelectorAll("nav > h1 > a")[0];
+let textoH1CAnvas = document.querySelectorAll(".off-canvas > h1 > a")[0];
 
 // console.log(textoH1);
-
+textoH1CAnvas.firstChild.textContent = "";
 textoH1.firstChild.textContent = "";
  
-//Codigo para añadir cosas
-console.log("hOLA");
+function menu() {
+    let navbar = document.querySelectorAll("nav")[0];
+    let nav_submenu = document.querySelectorAll(".submenu")[0];
+    let sticky = navbar.offsetTop;
+    console.log(nav_submenu);
 
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        nav_submenu.classList.add("stickymenu");
+    } else {
+        navbar.classList.remove("sticky");
+        nav_submenu.classList.remove("stickymenu");
+    }
+}
