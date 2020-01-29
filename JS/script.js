7038dad2f67975d8605e45d4dcd54082f2844101
Carlos Del Cura Pascual <delcuracarlos@gmail.com>
2020-01-29 12:22:00 +0100

    Arreglo de header hecho

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+let navbar = document.querySelectorAll("nav")[0];
+let navSubmenu = document.querySelectorAll("nav .menu ul")[0];
+let sticky = navbar.offsetTop;
 
 window.onscroll = () => {menu()};
 //Tareas al recargar la página
@@ -12,7 +15,7 @@ function tareasLoad() {
 function quitarTexto() {
     let textoClaseUl = document.querySelectorAll("aside > ul:first-child > li > a");
     let textoH1 = document.querySelectorAll("nav > h1 > a")[0];
-    let textoH1CAnvas = document.querySelectorAll(".off-canvas > h1 > a")[0];
+    let textoH1CAnvas = document.querySelectorAll(".off-canvas > div > h1 > a")[0];
     for (let index = 0; index < textoClaseUl.length; index++) {
         textoClaseUl[index].firstChild.textContent = "";
     }
@@ -20,11 +23,6 @@ function quitarTexto() {
     textoH1.firstChild.textContent = "";
 }
 function menu() {
-    let navbar = document.querySelectorAll("nav")[0];
-    let navSubmenu = document.querySelectorAll("nav .menu ul")[0];
-    console.log(navSubmenu);
-    
-    let sticky = navbar.offsetTop;
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
         navSubmenu.classList.remove("submenu");
@@ -33,18 +31,25 @@ function menu() {
         navbar.classList.remove("sticky");
         navSubmenu.classList.remove("stickymenu");
         navSubmenu.classList.add("submenu");
+        console.log("aquie");
+        
         // nav_submenu.classList.remove("stickymenu");
     }
 }
+
+let boton = document.querySelectorAll(".off-canvas > div > button")[0];
+let botonCerrar = document.getElementsByClassName("closebtn")[0];
+
+boton.addEventListener("click", ()=>{
+    openNav();
+});
+botonCerrar.addEventListener("click", () =>{
+    closebtn();
+});
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
+    document.getElementById("mySidenav").style.left = "0%";
+}
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  } 
+function closebtn() {
+    document.getElementById("mySidenav").style.left = "-100%";
+  }
